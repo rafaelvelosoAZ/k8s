0d1b6f374a7b9ed91c6b89ac0ede12cc02314858
rafael <rafa.v_s@hotmail.com>
2023-03-06 21:47:07 -0300

    add

--- main.sh
+++ main.sh
@@ -10,12 +10,12 @@ declare -rx STEPS=(
   'Install-Kubernetes-Components'
   )
 declare -rx CMDS=(
-  'source kubernetes-steps/.Update-all-Nodes.sh'
-  'source kubernetes-steps/.Add-etc-Modules.sh'
-  'source kubernetes-steps/.Update-Systemctl.sh'
-  'source kubernetes-steps/.Download-Docker.sh'
-  'source kubernetes-steps/.Install-Config-Containerd.sh'
-  'source kubernetes-steps/.Install-Kubernetes-Components.sh'
+  'source kubernetes-steps/Update-all-Nodes.sh'
+  'source kubernetes-steps/Add-etc-Modules.sh'
+  'source kubernetes-steps/Update-Systemctl.sh'
+  'source kubernetes-steps/Download-Docker.sh'
+  'source kubernetes-steps/Install-Config-Containerd.sh'
+  'source kubernetes-steps/Install-Kubernetes-Components.sh'
 )
 
 case $1 in
