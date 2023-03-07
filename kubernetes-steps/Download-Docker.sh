34b40aff17b9f35849df7447f30ff939478c2f01
rafael <rafa.v_s@hotmail.com>
2023-03-06 23:13:11 -0300

    add

--- kubernetes-steps/Download-Docker.sh
+++ kubernetes-steps/Download-Docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg &> /dev/null | sudo apt-key add - &> /dev/null
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &> /dev/null
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &> /dev/null
 
