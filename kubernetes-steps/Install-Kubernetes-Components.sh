d63ad3997796dcdac7b973687533c07e6ab1da1d
rafael <rafa.v_s@hotmail.com>
2023-03-07 21:46:44 -0300

    add

--- kubernetes-steps/Install-Kubernetes-Components.sh
+++ kubernetes-steps/Install-Kubernetes-Components.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-sudo apt-get update &> /dev/null && sudo apt-get install -y &> /dev/null apt-transport-https &> /dev/null gnupg2 &> /dev/null
+sudo apt-get update &> /dev/null
+sudo apt-get install -y &> /dev/null
+sudo apt-get install -y apt-transport-https &> /dev/null
+sudo apt-get install -y gnupg2 &> /dev/null
 
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - &> /dev/null
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add 
 
-sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list &> /dev/null
+sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update &> /dev/null
 
-sudo apt-get install -y kubelet &> /dev/null kubeadm &> /dev/null kubectl &> /dev/null
+sudo apt-get install -y kubelet &> /dev/null
+sudo apt-get install -y kubeadm &> /dev/null 
+sudo apt-get install -y kubectl &> /dev/null
 
-sudo swapoff -a &> /dev/null
+sudo swapoff -a
 
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab &> /dev/null
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
